75fb5dfa614b51b3e5a29cc76388d9ec102d577a
Brunokrk <45456467+Brunokrk@users.noreply.github.com>
2020-03-08 14:14:30 -0300

    1073
    Feito

---
+++ 1073/Ex.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int N, i, square;
+
+    scanf("%d", &N);
+    for (i = 1; i <= N; i++)
+    {
+        if ((i % 2) == 0)
+        {
+            square = pow(i, 2);
+            printf("%d^2 = %d\n", i, square);
+        }
+    }
+    return 0;
+}
